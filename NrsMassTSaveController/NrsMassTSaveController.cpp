8018a102ae64e98828d8fb80a50667537bed8e95
keitaronaruse <naruse@u-aizu.ac.jp>
2022-12-01 12:10:17 +0900

    updated

--- NrsMassTSaveController/NrsMassTSaveController.cpp
+++ NrsMassTSaveController/NrsMassTSaveController.cpp
@@ -3,59 +3,48 @@
 #include <iomanip>
 #include <iostream>
 
-class NrsMassTSaveController : public cnoid::SimpleController
-{
+class NrsMassTSaveController : public cnoid::SimpleController {
     cnoid::Link *t_link, *b_link;
     cnoid::SimpleControllerIO* io;
     std::ofstream ofs;
     double dt;
-    double t;
 
-public:
-    virtual bool initialize(cnoid::SimpleControllerIO* r_io) override
-    {
+   public:
+    virtual bool initialize( cnoid::SimpleControllerIO* r_io ) override {
         io = r_io;
-        b_link = io->body()->rootLink();
-        io->enableInput(b_link,
-                        cnoid::Link::LinkPosition | cnoid::Link::LinkTwist);
-        t_link = io->body()->link("Mass");
-        io->enableInput(t_link,
-                        cnoid::Link::LinkPosition | cnoid::Link::LinkTwist);
-        dt = io->timeStep();
+        b_link = io->body( )->rootLink( );
+        io->enableInput( b_link, cnoid::Link::LinkPosition | cnoid::Link::LinkTwist );
+        t_link = io->body( )->link( "Mass" );
+        io->enableInput( t_link, cnoid::Link::LinkPosition | cnoid::Link::LinkTwist );
 
         return true;
     }
 
-    virtual bool start() override
-    {
-        ofs = std::ofstream(io->optionString());
-        t = 0.0;
+    virtual bool start( ) override {
+        ofs = std::ofstream( io->optionString( ) );
         return true;
     }
 
-    virtual bool control() override
-    {
+    virtual bool control( ) override {
         //  Base link
-        cnoid::Isometry3 b_T = b_link->position();
-        cnoid::Vector3 b_t = b_T.translation();
-        cnoid::Matrix3 b_R = b_T.rotation();
+        cnoid::Isometry3 b_T = b_link->position( );
+        cnoid::Vector3 b_t = b_T.translation( );
+        cnoid::Matrix3 b_R = b_T.rotation( );
         //  Target link
-        cnoid::Isometry3 t_T = t_link->position();
-        cnoid::Vector3 t_t = t_T.translation();
-        cnoid::Matrix3 t_R = t_T.rotation();
-        cnoid::Vector3 t_dv = t_link->v();
-        cnoid::Vector3 t_dw = t_link->w();
+        cnoid::Isometry3 t_T = t_link->position( );
+        cnoid::Vector3 t_t = t_T.translation( );
+        cnoid::Matrix3 t_R = t_T.rotation( );
+        cnoid::Vector3 t_dv = t_link->v( );
+        cnoid::Vector3 t_dw = t_link->w( );
 
-        ofs << std::fixed << std::setprecision(6) << t << " " << b_t.x() << " "
-            << b_t.y() << " " << b_t.z() << " " << t_t.x() << " " << t_t.y()
-            << " " << t_t.z() << " " << t_dv.x() << " " << t_dv.y() << " "
-            << t_dv.z() << " " << std::endl;
+        ofs << std::fixed << std::setprecision( 6 ) << " " << b_t.x( ) << " " << b_t.y( ) << " " << b_t.z( ) << " "
+            << t_t.x( ) << " " << t_t.y( ) << " " << t_t.z( ) << " " << t_dv.x( ) << " " << t_dv.y( ) << " "
+            << t_dv.z( ) << " " << std::endl;
 
-        t += dt;
         return true;
     }
 
-    virtual void stop() override { ofs.close(); }
+    virtual void stop( ) override { ofs.close( ); }
 };
 
-CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(NrsMassTSaveController)
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY( NrsMassTSaveController )
